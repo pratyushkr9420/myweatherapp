3974259056419e21c1917ccec31a1d303be95fd0
Pratyush Kumar <kumarpratyush6294@gmail.com>
2024-03-07 13:04:20 -0500

    finished UI of HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,16 +6,23 @@ import {
   ImageBackground,
   TextInput,
   Pressable,
+  ScrollView,
 } from "react-native";
 import React, { useState } from "react";
-import { Feather, Entypo } from "@expo/vector-icons";
+import {
+  Feather,
+  Entypo,
+  MaterialCommunityIcons,
+  FontAwesome,
+  Fontisto,
+} from "@expo/vector-icons";
 
 const HomeScreen = () => {
   const [showSearch, setShowSearch] = useState(false);
   const [locations, setLocations] = useState([1, 2, 3]);
   const handleLocationUpdate = (location) => {};
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, padding: 0 }}>
       <ImageBackground
         style={{ flex: 1 }}
         source={require("../images/bgimage.jpg")}
@@ -26,7 +33,7 @@ const HomeScreen = () => {
             alignItems: "center",
             position: "absolute",
             justifyContent: "space-between",
-            top: 80,
+            top: 60,
           }}
         >
           <TextInput
@@ -42,7 +49,7 @@ const HomeScreen = () => {
           </Pressable>
         </View>
         {showSearch && (
-          <View style={{ marginTop: 130, marginLeft: 10 }}>
+          <View style={{ marginTop: 110, marginLeft: 10 }}>
             {locations.map((location, index) => (
               <Pressable
                 key={index}
@@ -52,7 +59,7 @@ const HomeScreen = () => {
                   width: "95%",
                   opacity: 0.4,
                   marginTop: 3.5,
-                  padding: 10,
+                  padding: 8,
                   flexDirection: "row",
                   alignItems: "center",
                 }}
@@ -64,7 +71,7 @@ const HomeScreen = () => {
                     marginLeft: 30,
                     color: "white",
                     fontWeight: "bold",
-                    fontSize: 20,
+                    fontSize: 16,
                   }}
                 >
                   London, United Kingom
@@ -91,15 +98,101 @@ const HomeScreen = () => {
           </View>
           <View style={styles.weatherIconandDegreeContainer}>
             <Image
-              style={{ width: 200, height: 200 }}
+              style={{ width: 150, height: 150 }}
               source={require("../images/sunny.png")}
             />
-            <Text style={{ color: "white", fontWeight: "bold", fontSize: 50 }}>
+            <Text
+              style={{
+                marginTop: 10,
+                color: "white",
+                fontWeight: "bold",
+                fontSize: 50,
+              }}
+            >
               25°C
             </Text>
-            <Text style={{ color: "white", fontWeight: "bold" }}>Sunny</Text>
+            <Text style={{ color: "white", fontWeight: "bold", fontSize: 20 }}>
+              Sunny
+            </Text>
+            <View style={styles.otherStatsContainer}>
+              <View style={{ flexDirection: "row" }}>
+                <MaterialCommunityIcons
+                  name="weather-windy"
+                  size={24}
+                  color="white"
+                />
+                <Text style={{ color: "white", fontWeight: "bold" }}>22km</Text>
+              </View>
+              <View style={{ flexDirection: "row" }}>
+                <Feather name="droplet" size={24} color="white" />
+                <Text style={{ color: "white", fontWeight: "bold" }}>20%</Text>
+              </View>
+              <View style={{ flexDirection: "row" }}>
+                <Feather name="sunrise" size={24} color="white" />
+                <Text style={{ color: "white", fontWeight: "bold" }}>
+                  6:10 AM
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 15,
+              marginTop: 40,
+              marginLeft: 20,
+            }}
+          >
+            <FontAwesome name="calendar" size={24} color="white" />
+            <Text style={{ color: "white", fontWeight: "bold", fontSize: 19 }}>
+              Daily forecast
+            </Text>
           </View>
         </View>
+        <ScrollView
+          style={{ position: "absolute", bottom: 50 }}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={styles.dailyForecastContainer}>
+            <Fontisto name="day-lightning" size={24} color="white" />
+            <Text style={{ color: "white", fontWeight: "bold" }}>Monday</Text>
+            <Text style={{ color: "white", fontWeight: "bold" }}>25°C</Text>
+          </View>
+          <View style={styles.dailyForecastContainer}>
+            <Fontisto name="day-lightning" size={24} color="white" />
+            <Text style={{ color: "white", fontWeight: "bold" }}>Tuesday</Text>
+            <Text style={{ color: "white", fontWeight: "bold" }}>25°C</Text>
+          </View>
+          <View style={styles.dailyForecastContainer}>
+            <Fontisto name="day-lightning" size={24} color="white" />
+            <Text style={{ color: "white", fontWeight: "bold" }}>
+              Wednesday
+            </Text>
+            <Text style={{ color: "white", fontWeight: "bold" }}>25°C</Text>
+          </View>
+          <View style={styles.dailyForecastContainer}>
+            <Fontisto name="day-lightning" size={24} color="white" />
+            <Text style={{ color: "white", fontWeight: "bold" }}>Thursday</Text>
+            <Text style={{ color: "white", fontWeight: "bold" }}>25°C</Text>
+          </View>
+          <View style={styles.dailyForecastContainer}>
+            <Fontisto name="day-lightning" size={24} color="white" />
+            <Text style={{ color: "white", fontWeight: "bold" }}>Friday</Text>
+            <Text style={{ color: "white", fontWeight: "bold" }}>25°C</Text>
+          </View>
+          <View style={styles.dailyForecastContainer}>
+            <Fontisto name="day-lightning" size={24} color="white" />
+            <Text style={{ color: "white", fontWeight: "bold" }}>Saturday</Text>
+            <Text style={{ color: "white", fontWeight: "bold" }}>25°C</Text>
+          </View>
+          <View style={styles.dailyForecastContainer}>
+            <Fontisto name="day-lightning" size={24} color="white" />
+            <Text style={{ color: "white", fontWeight: "bold" }}>Sunday</Text>
+            <Text style={{ color: "white", fontWeight: "bold" }}>25°C</Text>
+          </View>
+        </ScrollView>
       </ImageBackground>
     </View>
   );
@@ -125,9 +218,11 @@ const styles = StyleSheet.create({
     opacity: 0.3,
   },
   forecastSectionContainer: {
+    top: 260,
+    right: 10,
     position: "absolute",
-    top: 300,
-    left: "15%",
+    width: "100%",
+    paddingHorizontal: 2,
   },
   forecastSectionTextContainer: {
     flexDirection: "row",
@@ -135,9 +230,28 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   weatherIconandDegreeContainer: {
-    marginTop: 50,
+    marginTop: 30,
     marginLeft: 30,
     alignItems: "center",
     gap: 20,
+  },
+  otherStatsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 60,
+  },
+  dailyForecastContainer: {
+    marginLeft: 20,
+    padding: 10,
+    borderRadius: 20,
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3, // Adjust the shadow opacity
+    shadowRadius: 3,
+    elevation: 5,
   },
 });
