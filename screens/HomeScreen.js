c02077b24b43c681d85f027d3d39c5439fa7beba
Pratyush Kumar <kumarpratyush6294@gmail.com>
2024-03-08 11:49:31 -0500

    added screenshots demonstrating app functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,8 +7,10 @@ import {
   TextInput,
   Pressable,
   ScrollView,
+  ActivityIndicator,
+  SafeAreaView,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Feather,
   Entypo,
@@ -23,6 +25,7 @@ const HomeScreen = () => {
   const [showSearch, setShowSearch] = useState(false);
   const [locations, setLocations] = useState([]);
   const [currentLocation, setCurrentLocation] = useState({});
+  const [loading, setLoading] = useState(true);
   const daysOfWeek = [
     "Sunday",
     "Monday",
@@ -36,7 +39,6 @@ const HomeScreen = () => {
     if (searchValue.length > 2) {
       setTimeout(() => {
         fetchLocation(searchValue, REACT_APP_API_KEY).then((data) => {
-          console.log(data[0]);
           setLocations(data);
         });
       }, 3000);
@@ -45,173 +47,206 @@ const HomeScreen = () => {
 
   const handleLocationUpdate = (location) => {
     setLocations([]);
+    setLoading(true);
     fetchWeatherForecast(location.name, 7, REACT_APP_API_KEY).then((data) => {
       setCurrentLocation(data);
+      setLoading(false);
       console.log("The current data is", data);
     });
     setShowSearch((currentState) => !currentState);
   };
+  const initalDataFetch = () => {
+    fetchWeatherForecast("Boston", 7, REACT_APP_API_KEY).then((data) => {
+      console.log(data.forecast.forecastday);
+      //currentLocation?.forecast?.forecastday
+      // currentLocation?.current?.condition?.wind_kph
+      setCurrentLocation(data);
+    });
+    setLoading(false);
+  };
+  useEffect(() => {
+    initalDataFetch();
+  }, []);
   return (
     <View style={{ flex: 1, padding: 0 }}>
       <ImageBackground style={{ flex: 1 }} source={require("../images/bg.jpg")}>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            position: "absolute",
-            justifyContent: "space-between",
-            top: 60,
-          }}
-        >
-          <TextInput
-            onChangeText={(text) => handleSearch(text)}
-            placeholderTextColor="rgba(255, 255, 255, 0.7)"
-            style={styles.searchBar}
-            placeholder="Search city"
-          />
-          <Pressable
-            onPress={() => setShowSearch((currentState) => !currentState)}
-            style={{ position: "absolute", top: 10, right: 22 }}
-          >
-            <Entypo name="magnifying-glass" size={24} color="white" />
-          </Pressable>
-        </View>
-        {showSearch && (
-          <View
-            style={{
-              marginTop: 110,
-              marginLeft: 10,
-            }}
-          >
-            {locations.map((location, index) => (
+        {loading ? (
+          <View>
+            <ActivityIndicator size="large" />
+          </View>
+        ) : (
+          <SafeAreaView>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                position: "absolute",
+                justifyContent: "space-between",
+                top: 60,
+              }}
+            >
+              <TextInput
+                onChangeText={(text) => handleSearch(text)}
+                placeholderTextColor="rgba(255, 255, 255, 0.7)"
+                style={styles.searchBar}
+                placeholder="Search city"
+              />
               <Pressable
-                key={index}
-                style={{
-                  backgroundColor: "gray",
-                  borderRadius: 20,
-                  width: "95%",
-                  opacity: 1.0,
-                  marginTop: 2.0,
-                  marginLeft: 5,
-                  padding: 8,
-                  flexDirection: "row",
-                  alignItems: "center",
-                }}
-                onPress={() => handleLocationUpdate(location)}
+                onPress={() => setShowSearch((currentState) => !currentState)}
+                style={{ position: "absolute", top: 10, right: 22 }}
               >
-                <Feather name="map-pin" size={24} color="white" />
+                <Entypo name="magnifying-glass" size={24} color="white" />
+              </Pressable>
+            </View>
+            {showSearch && (
+              <View
+                style={{
+                  marginTop: 60,
+                  marginLeft: 10,
+                }}
+              >
+                {locations.slice(0, 3).map((location, index) => (
+                  <Pressable
+                    key={index}
+                    style={{
+                      backgroundColor: "gray",
+                      borderRadius: 20,
+                      width: "95%",
+                      opacity: 1.0,
+                      marginLeft: 5,
+                      marginTop: 2,
+                      padding: 8,
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                    onPress={() => handleLocationUpdate(location)}
+                  >
+                    <Feather name="map-pin" size={24} color="white" />
+                    <Text
+                      style={{
+                        marginLeft: 30,
+                        color: "white",
+                        fontWeight: "bold",
+                        fontSize: 16,
+                      }}
+                    >
+                      {location?.name},{location?.country}
+                    </Text>
+                  </Pressable>
+                ))}
+              </View>
+            )}
+            <View style={styles.forecastSectionContainer}>
+              <View style={styles.forecastSectionTextContainer}>
+                <Text
+                  style={{ color: "white", fontSize: 30, fontWeight: "bold" }}
+                >
+                  {currentLocation?.location?.name},
+                </Text>
                 <Text
                   style={{
-                    marginLeft: 30,
                     color: "white",
-                    fontWeight: "bold",
-                    fontSize: 16,
+                    fontSize: 23,
+                    fontWeight: "400",
+                    marginTop: 5,
                   }}
                 >
-                  {location?.name},{location?.country}
+                  {currentLocation?.location?.country}
                 </Text>
-              </Pressable>
-            ))}
-          </View>
-        )}
-        <View style={styles.forecastSectionContainer}>
-          <View style={styles.forecastSectionTextContainer}>
-            <Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>
-              {currentLocation?.location?.name},
-            </Text>
-            <Text
-              style={{
-                color: "white",
-                fontSize: 23,
-                fontWeight: "400",
-                marginTop: 5,
-              }}
-            >
-              {currentLocation?.location?.country}
-            </Text>
-          </View>
-          <View style={styles.weatherIconandDegreeContainer}>
-            <Image
-              style={{ width: 150, height: 150 }}
-              source={{
-                uri: "http:" + currentLocation?.current?.condition?.icon,
-              }}
-            />
-            <Text
-              style={{
-                marginTop: 10,
-                color: "white",
-                fontWeight: "bold",
-                fontSize: 50,
-              }}
-            >
-              {currentLocation?.current?.temp_c}°C
-            </Text>
-            <Text style={{ color: "white", fontWeight: "bold", fontSize: 20 }}>
-              {currentLocation?.current?.condition?.text}
-            </Text>
-            <View style={styles.otherStatsContainer}>
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <MaterialCommunityIcons
-                  name="weather-windy"
-                  size={24}
-                  color="white"
+              </View>
+              <View style={styles.weatherIconandDegreeContainer}>
+                <Image
+                  style={{ width: 150, height: 150 }}
+                  source={{
+                    uri: "http:" + currentLocation?.current?.condition?.icon,
+                  }}
                 />
-                <Text style={{ color: "white", fontWeight: "bold" }}>
-                  {currentLocation?.current?.condition?.wind_kph}km
+                <Text
+                  style={{
+                    marginTop: 10,
+                    color: "white",
+                    fontWeight: "bold",
+                    fontSize: 50,
+                  }}
+                >
+                  {currentLocation?.current?.temp_c}°C
                 </Text>
-              </View>
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <Feather name="droplet" size={24} color="white" />
-                <Text style={{ color: "white", fontWeight: "bold" }}>
-                  {currentLocation?.current?.condition?.humidity}%
+                <Text
+                  style={{ color: "white", fontWeight: "bold", fontSize: 20 }}
+                >
+                  {currentLocation?.current?.condition?.text}
                 </Text>
+                <View style={styles.otherStatsContainer}>
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <MaterialCommunityIcons
+                      name="weather-windy"
+                      size={24}
+                      color="white"
+                    />
+                    <Text style={{ color: "white", fontWeight: "bold" }}>
+                      {currentLocation?.current?.wind_kph}km
+                    </Text>
+                  </View>
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Feather name="droplet" size={24} color="white" />
+                    <Text style={{ color: "white", fontWeight: "bold" }}>
+                      {currentLocation?.current?.humidity}%
+                    </Text>
+                  </View>
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Feather name="sunrise" size={24} color="white" />
+                    <Text style={{ color: "white", fontWeight: "bold" }}>
+                      {
+                        currentLocation?.forecast?.forecastday[0]?.astro
+                          ?.sunrise
+                      }
+                    </Text>
+                  </View>
+                </View>
               </View>
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <Feather name="sunrise" size={24} color="white" />
-                <Text style={{ color: "white", fontWeight: "bold" }}>
-                  {currentLocation?.forecast?.forecastday[0]?.astro?.sunrise}
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 15,
+                  marginTop: 40,
+                  marginLeft: 35,
+                }}
+              >
+                <FontAwesome name="calendar" size={24} color="white" />
+                <Text
+                  style={{ color: "white", fontWeight: "bold", fontSize: 19 }}
+                >
+                  Daily forecast
                 </Text>
               </View>
             </View>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 15,
-              marginTop: 40,
-              marginLeft: 35,
-            }}
-          >
-            <FontAwesome name="calendar" size={24} color="white" />
-            <Text style={{ color: "white", fontWeight: "bold", fontSize: 19 }}>
-              Daily forecast
-            </Text>
-          </View>
-        </View>
-        <ScrollView
-          style={{ position: "absolute", bottom: 70 }}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-        >
-          {currentLocation?.forecast?.forecastday.map((day, index) => {
-            const currentDayObject = new Date(day.date);
-            const currentDay = daysOfWeek[currentDayObject.getDay()];
-            return (
-              <View key={index} style={styles.dailyForecastContainer}>
-                <Image
-                  style={{ width: 50, height: 50 }}
-                  source={{ uri: "http:" + day?.day?.condition?.icon }}
-                />
-                <Text style={{ color: "white", fontWeight: "bold" }}>
-                  {currentDay}
-                </Text>
-                <Text style={{ color: "white", fontWeight: "bold" }}>25°C</Text>
-              </View>
-            );
-          })}
-        </ScrollView>
+            <ScrollView
+              style={{ position: "absolute", top: 750 }}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            >
+              {currentLocation?.forecast?.forecastday.map((day, index) => {
+                const currentDayObject = new Date(day.date);
+                const currentDay = daysOfWeek[currentDayObject.getDay()];
+                return (
+                  <View key={index} style={styles.dailyForecastContainer}>
+                    <Image
+                      style={{ width: 50, height: 50 }}
+                      source={{ uri: "http:" + day?.day?.condition?.icon }}
+                    />
+                    <Text style={{ color: "white", fontWeight: "bold" }}>
+                      {currentDay}
+                    </Text>
+                    <Text style={{ color: "white", fontWeight: "bold" }}>
+                      {day?.day?.avgtemp_c}°C
+                    </Text>
+                  </View>
+                );
+              })}
+            </ScrollView>
+          </SafeAreaView>
+        )}
       </ImageBackground>
     </View>
   );
